7e5998762e2e306c4a2ce8c9e5ee48c08fd3f26e
Gabrielle <ms.grodarte@gmail.com>
2025-04-01 14:24:48 -0700

    will add signup feature

--- server/app.py
+++ server/app.py
@@ -3,12 +3,31 @@
 from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import NotFound
 
 from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    
+    def post(self):
+        json = request.get_json()
+        username = json.get('username')
+        if User.query.filter_by(username=username).first():
+            return {"error": "Username already exists"}, 422
+        try:
+            user = User(username=json.get('username'))
+            user.password_hash = json.get('password')
+            user.image_url = json.get('image_url')
+            user.bio = json.get('bio')
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return user.to_dict(), 201
+        except Exception as e:
+            return {"error": "An error occurred during signup"}, 422
+
+
 
 class CheckSession(Resource):
     pass
